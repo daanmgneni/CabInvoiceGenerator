a6f5b4f5858fd2825044927946f52b8db96c32d4
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-04-22 08:10:18 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,5 +5,11 @@ namespace CabInvoiceGenerator
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Cab Invoice Generator");
-          
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+
+            Console.WriteLine(fare);
+
+           
         }
