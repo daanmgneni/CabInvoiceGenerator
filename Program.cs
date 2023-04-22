fb21ecf1be6d7b9f0247b1aa25ddf6a7520bae06
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-04-22 08:08:19 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+          
+        }
